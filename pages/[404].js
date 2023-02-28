2625f874a4f61070b7e8909674d756b7ced75e76
Admin <ashishnakrani606@gmail.com>
2023-02-28 18:42:37 +0530

    feat: added 404 page.

---
+++ pages/[404].js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Layout from "@/layouts/layout";
+
+const FourOhFour = () => {
+ 
+  return (
+    <Layout>
+      Page Not Found
+    </Layout>
+  );
+};
+
+export default FourOhFour;
